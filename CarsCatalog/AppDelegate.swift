379eed4a7c6a1f7523a41ea230e37864f89636b5
Михаил Жданов <mikhail.zhdanov@digital-ecosystems.ru>
2020-09-18 22:08:39 +0700

    Created project

---
+++ CarsCatalog/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CarsCatalog
+//
+//  Created by Михаил Жданов on 18.09.2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
